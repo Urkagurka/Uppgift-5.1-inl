a76f306f8d720a69e2e1a6c3b3ee9dda272e8e70
Urkagurka <143172181+Urkagurka@users.noreply.github.com>
2023-11-22 10:32:12 +0100

    Add project files.

---
+++ Uppgift 5.1 inl/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Uppgfit_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in 5 namn");
+            string[] namn = new string[6];
+            for (int i = 1; i <= 5; i++)
+            {
+                namn[i] = Console.ReadLine();
+            }
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(namn[i]);
+            }
+            Console.Read();
+        }
+    }
+}
